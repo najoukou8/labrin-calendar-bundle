e42274b95f0fcb810b29baaff634c6aec1f31f0a
najoukou8 <l.najla1999@hotmail.fr>
2025-06-24 14:49:47 +0200

    namespace modified

--- src/LabrinCalendarBundle.php
+++ src/LabrinCalendarBundle.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Labrin\CalendarBundle\src;
+namespace Labrin\CalendarBundle;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
