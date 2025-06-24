70db5191ca250d999c79003f91f47bf3f2d92958
najoukou8 <l.najla1999@hotmail.fr>
2025-06-24 13:59:23 +0200

    folder src added

--- src/LabrinCalendarBundle.php
+++ src/LabrinCalendarBundle.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Labrin\CalendarBundle;
+namespace Labrin\CalendarBundle\src;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
