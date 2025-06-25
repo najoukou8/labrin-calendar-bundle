dc38d1126f6595d2de601a90baa14ef0467ca8cb
najoukou8 <l.najla1999@hotmail.fr>
2025-06-25 12:02:30 +0200

    bundle modified

--- src/LabrinCalendarBundle.php
+++ src/LabrinCalendarBundle.php
@@ -2,20 +2,30 @@
 
 namespace Labrin\CalendarBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 
-class LabrinCalendarBundle extends Bundle
+class LabrinCalendarBundle extends Bundle  implements PrependExtensionInterface
 {
     public function getPath(): string
     {
         return \dirname(__DIR__);
     }
-
+    public function prepend(ContainerBuilder $container)
+    {
+        // Add Twig namespace mapping automatically
+        $container->prependExtensionConfig('twig', [
+            'paths' => [
+                __DIR__ . '/../Resources/views' => 'Calendar'
+            ],
+        ]);
+    }
     public function configureRoutes(RoutingConfigurator $routes): void
     {
-        $routes->import(__DIR__.'/Resources/config/routes.yaml')
+        $routes->import(__DIR__.'/../Resources/config/routes.yaml')
             ->prefix('/calendar');
     }
 }
